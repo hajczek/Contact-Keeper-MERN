0b77de2061e1e82095eac4786833b77a0ce99b70
hajczek <i.hajczewska@gmail.com>
2020-08-14 15:20:31 +0200

    Added condition for not match password

--- routes/auth.js
+++ routes/auth.js
@@ -37,6 +37,10 @@ router.post(
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid Credentials" });
+      }
     } catch (err) {}
   }
 );
