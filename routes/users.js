a7acf74d1ed0adff0e8ab02a5cef96270b3122bd
hajczek <i.hajczewska@gmail.com>
2020-08-14 13:49:57 +0200

    Added bcrypt option for hash password before sending data on server

--- routes/users.js
+++ routes/users.js
@@ -39,6 +39,12 @@ router.post(
         email,
         password,
       });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
     } catch (err) {}
   }
 );
