d480f6dc32671da1e888444b4238bdd263b0f6f2
hajczek <i.hajczewska@gmail.com>
2020-08-14 15:16:32 +0200

    Defined needed variables for auth user - email, password; added try catch block for checking user

--- routes/auth.js
+++ routes/auth.js
@@ -21,11 +21,21 @@ router.post(
     check("email", "Please include a valid email").isEmail(),
     check("password", "Password is required").exists(),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid Credentials" });
+      }
+    } catch (err) {}
   }
 );
 
