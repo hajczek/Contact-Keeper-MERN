6ba0bb5a70b9f343b9d88c72d927f4a1a3c934ef
hajczek <i.hajczewska@gmail.com>
2020-08-14 13:18:52 +0200

    Added options for checking user data

--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,21 @@ const User = require("../models/User");
 // @route       POST api/users
 // @desc        Register a user
 // @access      Public
-router.post("/", (req, res) => {
-  res.send(req.body);
-});
+router.post(
+  "/",
+  [
+    check("name", "Please add name").not().isEmpty(),
+    check("email", "Please include invalid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    res.send(req.body);
+  }
+);
 
 module.exports = router;
