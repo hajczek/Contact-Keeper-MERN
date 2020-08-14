65e7fd1d793f6be953a7fcedb2f26eb4b4838ef9
hajczek <i.hajczewska@gmail.com>
2020-08-14 14:17:00 +0200

    Define jwt

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../models/Users");
