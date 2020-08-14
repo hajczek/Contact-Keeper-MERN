fbba243d05201546b4a8f4f6e5169cc38cf74ed6
hajczek <i.hajczewska@gmail.com>
2020-08-14 14:06:12 +0200

    Fixed mistake with name element in Schema User

--- models/Users.js
+++ models/Users.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
-  user: {
+  name: {
     type: String,
     required: true,
   },
--- routes/users.js
+++ routes/users.js
@@ -26,7 +26,7 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { name, email, passowrd } = req.body;
+    const { name, email, password } = req.body;
 
     try {
       let user = await User.findOne({ email });
