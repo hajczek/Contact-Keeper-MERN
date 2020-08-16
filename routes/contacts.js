f5b7cefe054e2a71ebead542744ab025aa0ef861
hajczek <i.hajczewska@gmail.com>
2020-08-16 11:28:59 +0200

    Added needed things for actions with contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../models/Users");
+const Contact = require("../models/Contacts");
 
 // @route       GET api/contacts
 // @desc        Get all user contacts
