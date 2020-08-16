ab36cc80e6f72b3a9a5e88bcc7e9db2aa621f6ea
hajczek <i.hajczewska@gmail.com>
2020-08-16 12:36:28 +0200

    Fixed typo

--- routes/contacts.js
+++ routes/contacts.js
@@ -101,7 +101,7 @@ router.delete("/:id", auth, async (req, res) => {
       return res.status(401).json({ msg: "Not authorized" });
     }
 
-    await Contact.findByIdAndRemove(req.params.ids);
+    await Contact.findByIdAndRemove(req.params.id);
 
     res.json({ msg: "Contact removed" });
   } catch (err) {
