0fe067b33459574427c579c361fe5745456a79bd
hajczek <i.hajczewska@gmail.com>
2020-08-16 11:53:38 +0200

    Added async option

--- routes/contacts.js
+++ routes/contacts.js
@@ -27,7 +27,7 @@ router.get("/", auth, async (req, res) => {
 router.post(
   "/",
   [auth, [check("name", "Name is required").now().isEmpty()]],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -41,13 +41,12 @@ router.post(
         email,
         phone,
         type,
-        user: req.user.id
+        user: req.user.id,
       });
 
       const contact = await newContact.save();
       res.json(contact);
-    }
-    catch(err){
+    } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
     }
