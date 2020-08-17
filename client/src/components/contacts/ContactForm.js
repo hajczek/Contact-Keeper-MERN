be1c4c1538a88c8eb1f159c464813c7e352a4eb5
hajczek <i.hajczewska@gmail.com>
2020-08-17 11:26:03 +0200

    Added component for contact form

---
+++ client/src/components/contacts/ContactForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+const ContactForm = () => {
+  const [contact, setContact] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    type: "",
+  });
+
+  const { name, email, phone, type } = contact;
+
+  return (
+    <form>
+      <h2>Add Contact</h2>
+      <input type="text" placeholder="Name" value={name} onChange={onChange} />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        placeholder="Phone"
+        value={phone}
+        onChange={onChange}
+      />
+      <h5>Contact Type</h5>
+      <input
+        type="radio"
+        name="type"
+        value="personal"
+        checked={type === "personal"}
+      />{" "}
+      Personal{" "}
+      <input
+        type="radio"
+        name="type"
+        value="professional"
+        checked={type === "professional"}
+      />{" "}
+      Professional
+      <div>
+        <input type="submit" value="Add Contact" />
+      </div>
+    </form>
+  );
+};
+
+export default ContactForm;
