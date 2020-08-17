63d9b1108b313f0f4d1ac4dffab45b0399b5c6d6
hajczek <i.hajczewska@gmail.com>
2020-08-17 16:34:12 +0200

    Added function deleteContact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -47,6 +47,9 @@ const ContactState = (props) => {
   };
 
   // Delete Contact
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
 
   // Set Current ContactState
 
