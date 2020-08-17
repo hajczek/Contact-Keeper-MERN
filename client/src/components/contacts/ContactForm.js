f448cb99df2821b6d912320209a2f91b46391eec
hajczek <i.hajczewska@gmail.com>
2020-08-17 12:06:42 +0200

    Fixed typo

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -30,16 +30,24 @@ const ContactForm = () => {
   return (
     <form onSubmit={onSubmit}>
       <h2>Add Contact</h2>
-      <input type="text" placeholder="Name" value={name} onChange={onChange} />
+      <input
+        type="text"
+        placeholder="Name"
+        name="name"
+        value={name}
+        onChange={onChange}
+      />
       <input
         type="email"
         placeholder="Email"
+        name="email"
         value={email}
         onChange={onChange}
       />
       <input
         type="text"
         placeholder="Phone"
+        name="phone"
         value={phone}
         onChange={onChange}
       />
@@ -61,7 +69,8 @@ const ContactForm = () => {
       />{" "}
       Professional
       <div>
-        <input type="submit" value="Add Contact" />
+        {/* <input type="submit" value="Add Contact" /> */}
+        <button>Add Contact</button>
       </div>
     </form>
   );
