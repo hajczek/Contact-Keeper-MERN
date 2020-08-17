68c93592607385e0230f2380f8c4ce4b5e29a166
hajczek <i.hajczewska@gmail.com>
2020-08-17 11:05:36 +0200

    Added local styles for div with contact details

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -3,10 +3,21 @@ import React from "react";
 const ContactItem = ({ contact }) => {
   const { id, name, email, phone, type } = contact;
   return (
-    <div>
+    <div
+      style={{
+        display: "block",
+        width: "250px",
+        backgroundColor: "#ddd",
+        padding: "20px",
+        marginBottom: "10px",
+      }}
+    >
       <h3>
         {name}{" "}
-        <span className={type === "professional" ? "success" : "primary"}>
+        <span
+          style={{ float: "right" }}
+          className={type === "professional" ? "success" : "primary"}
+        >
           {type.charAt(0).toUpperCase() + type.slice(1)}
         </span>
       </h3>
