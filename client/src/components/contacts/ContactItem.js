1482df8bc766b8b3874bd9d3ada1ddb281a237c9
hajczek <i.hajczewska@gmail.com>
2020-08-17 11:02:34 +0200

    Made first letter of type name capital

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -7,7 +7,7 @@ const ContactItem = ({ contact }) => {
       <h3>
         {name}{" "}
         <span className={type === "professional" ? "success" : "primary"}>
-          {type}
+          {type.charAt(0).toUpperCase() + type.slice(1)}
         </span>
       </h3>
     </div>
