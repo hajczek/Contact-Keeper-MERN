08604579f63eddafe6ed1e75c771f92e5cf03808
hajczek <i.hajczewska@gmail.com>
2020-08-17 11:28:31 +0200

    Added onChange code

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -10,6 +10,9 @@ const ContactForm = () => {
 
   const { name, email, phone, type } = contact;
 
+  const onChange = (e) =>
+    setContact({ ...contact, [e.target.name]: e.target.value });
+
   return (
     <form>
       <h2>Add Contact</h2>
