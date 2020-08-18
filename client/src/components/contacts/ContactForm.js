45cea9d6f4862661804070b8fec94c4bd1b5efe0
hajczek <i.hajczewska@gmail.com>
2020-08-18 16:07:22 +0200

    Added import for useEffect hook

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import ContactContext from "../../context/contact/contactContext";
 
 const ContactForm = () => {
