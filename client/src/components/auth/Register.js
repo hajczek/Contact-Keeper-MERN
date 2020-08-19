8ae7f9b14ca36146c8644013fe91ebfee4b57ce8
hajczek <i.hajczewska@gmail.com>
2020-08-19 11:27:49 +0200

    Added structure of register form

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const Register = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = user;
+
+  return (
+    <div>
+      <h1>Account Register</h1>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" value={name} onChange={onChange} />
+        </div>
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input type="email" name="email" value={email} onChange={onChange} />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password2">Confirm Password</label>
+          <input
+            type="password"
+            name="password2"
+            value={password2}
+            onChange={onChange}
+          />
+        </div>
+        <input type="submit" value="Register" />
+      </form>
+    </div>
+  );
+};
+export default Register;
