f4e8a16afa3765e813d44b9569f601f2b4ef52b8
hajczek <i.hajczewska@gmail.com>
2020-08-21 18:04:21 +0200

    Remove mistake automatically set by VSC

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -10,7 +10,7 @@ import {
 } from "../types";
 
 export default (state, action) => {
-  switch (OfflineAudioCompletionEvent.type) {
+  switch (action.type) {
     case REGISTER_SUCCESS:
       localStorage.setItem("token", action.payload.token);
       return {
