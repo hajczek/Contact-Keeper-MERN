779c586f14b0087f10ec4b4b53a3788d54f38ce9
hajczek <i.hajczewska@gmail.com>
2020-08-23 12:01:12 +0200

    Added functionality for login function

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -66,7 +66,27 @@ const AuthState = (props) => {
   };
 
   // Login User
-  const login = () => console.log("login");
+  const login = async (formData) => {
+    const config = {
+      headers: {
+        "Context-Type": "Application/json",
+      },
+    };
+    try {
+      const res = await axios.post("/api/auth", formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
+  };
 
   // Logout
   const logout = () => console.log("logut");
