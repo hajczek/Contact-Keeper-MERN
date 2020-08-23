ecf5ab9b9b3f85c1b124f514143f54b46b843c57
hajczek <i.hajczewska@gmail.com>
2020-08-23 12:48:49 +0200

    Added functionality for logout option

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -89,7 +89,7 @@ const AuthState = (props) => {
   };
 
   // Logout
-  const logout = () => console.log("logut");
+  const logout = () => dispatch({ type: LOGOUT });
 
   // Clear Errors
   const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
