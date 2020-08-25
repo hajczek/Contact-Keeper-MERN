c18b4ea7c0d5acf06e227085cdf416d6bba18a30
hajczek <i.hajczewska@gmail.com>
2020-08-25 16:08:55 +0200

    Brought getContacts and loading and call getContacts in useEffect

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,4 @@
-import React, { useContext, Fragment } from "react";
+import React, { useContext, Fragment, useEffect } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ContactContext from "../../context/contact/contactContext";
 import ContactItem from "./ContactItem";
@@ -6,7 +6,14 @@ import ContactItem from "./ContactItem";
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts, filtered } = contactContext;
+  const { contacts, filtered, getContacts, loading } = contactContext;
+
+  useEffect =
+    (() => {
+      getContacts();
+      //eslint-disable-next-line
+    },
+    []);
 
   if (contacts.length === 0) {
     return <h4>Please add contact</h4>;
