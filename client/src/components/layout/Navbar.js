44bf238cff56e47028bf153a1555f0172558aa88
hajczek <i.hajczewska@gmail.com>
2020-08-26 09:45:10 +0200

    Added base styles css and needed name of classes for navbar element

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,8 +1,8 @@
-import React, { Fragment, useContext } from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-import AuthContext from "../../context/auth/authContext";
-import ContactContext from "../../context/contact/contactContext";
+import React, { Fragment, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
+import ContactContext from '../../context/contact/contactContext';
 
 const Navbar = ({ title, icon }) => {
   const authContext = useContext(AuthContext);
@@ -20,8 +20,8 @@ const Navbar = ({ title, icon }) => {
     <Fragment>
       <li>Hello {user && user.name}</li>
       <li>
-        <a onClick={onLogout} href="#!">
-          <i className="fas fa-sign-out-alt" /> Logout
+        <a onClick={onLogout} href='#!'>
+          <i className='fas fa-sign-out-alt' /> Logout
         </a>
       </li>
     </Fragment>
@@ -30,19 +30,18 @@ const Navbar = ({ title, icon }) => {
   const guestLinks = (
     <Fragment>
       <li>
-        <Link to="/register">Register</Link>
+        <Link to='/register'>Register</Link>
       </li>
       <li>
-        <Link to="/login">Login</Link>
+        <Link to='/login'>Login</Link>
       </li>
     </Fragment>
   );
 
   return (
-    <div>
+    <div className='navbar'>
       <h1>
-        <i className={icon} />
-        {title}
+        <i className={icon} /> {title}
       </h1>
       <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
     </div>
@@ -55,7 +54,7 @@ Navbar.propTypes = {
 };
 
 Navbar.defaultProps = {
-  title: "Contact Keeper",
-  icon: "fas fa-id-card-alt",
+  title: 'Contact Keeper',
+  icon: 'fas fa-id-card-alt',
 };
 export default Navbar;
