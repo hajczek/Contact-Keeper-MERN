ed9f3d77efc2fcd88a48761ba50901d32f0e9598
hajczek <i.hajczewska@gmail.com>
2020-08-26 17:14:43 +0200

    Removed not needed part of code

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const connectDB = require("./config/db");
+const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
@@ -9,14 +9,10 @@ connectDB();
 // Init Middleware
 app.use(express.json({ expended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the ContactKeeper API" })
-);
-
 // Define Routes
-app.use("/api/users", require("./routes/users"));
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/contacts", require("./routes/contacts"));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
